03d44d1cdc575be8343a2f4d6a3e80634e31b9b2
Zachary Kipping <zjkipping@hotmail.com>
2019-04-11 00:10:31 -0500

    Employees dashboard UI tweaks
    also fixed the employee id display on order statuses

--- src/app/employees/order-status-list/order-status/order-status.component.ts
+++ src/app/employees/order-status-list/order-status/order-status.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { Order } from '@types';
 import {
@@ -6,18 +7,32 @@ import {
   DELIVERY_ORDER_STATUS,
   PICKUP_ORDER_STATUS,
 } from '@constants';
+import { BarTapApi } from '@api';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-order-status',
   templateUrl: './order-status.component.html',
   styleUrls: ['./order-status.component.scss']
 })
-export class OrderStatusComponent {
+export class OrderStatusComponent implements OnInit {
   @Input() order?: Order;
+  @Input() barId?: string;
+  employeeId?: Observable<string>;
 
   statusDisplay: { [key: string]: string } = {
     [IN_PROGRESS_ORDER_STATUS]: 'In Progress',
     [DELIVERY_ORDER_STATUS]: 'Delivering',
     [PICKUP_ORDER_STATUS]: 'Pickup',
   };
+
+  constructor(private api: BarTapApi) { }
+
+  ngOnInit() {
+    if (this.order && this.barId) {
+      this.employeeId = this.api.getBarEmployee(this.barId, this.order.employeeId as string).pipe(
+        map(employee => employee ? employee.id : 'no_employee')
+      );
+    }
+  }
 }
